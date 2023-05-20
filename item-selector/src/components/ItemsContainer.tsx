26c6446aeb32635f209d2a4d7fd04bbeb1b9e800
djk01281 <djk01281@gmail.com>
2023-05-20 01:30:16 +0000

    chore: create items.json data

--- item-selector/src/components/ItemsContainer.tsx
+++ item-selector/src/components/ItemsContainer.tsx
@@ -1,7 +1,7 @@
 import Item from './Item'
-
+import itemsData from '../data/items.json'
 const ItemsContainer = () =>{
-    const itemsList:string[] = ["item1", "item2", "item3"];
+    const itemsList:string[] = itemsData.itemsArray;
     return <div className = 'ItemsContainer'>{itemsList.map((item)=>(
         <Item name={item}></Item>
     ))}</div>
