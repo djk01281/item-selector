bdfbc6d4658e3e7b24824c2addbac3bd1971ec1c
djk01281 <djk01281@gmail.com>
2023-05-20 00:45:18 +0000

    feat: create basic Items

--- item-selector/src/ItemsContainer.tsx
+++ item-selector/src/ItemsContainer.tsx
@@ -1,7 +1,13 @@
 
 const ItemsContainer = () =>{
+    const itemsList:string[] = ["item1", "item2", "item3"];
+    return <div className = 'ItemsContainer'>{itemsList.map((item)=>(
+        <Item name={item}></Item>
+    ))}</div>
+}
 
-    return <div className = 'ItemsContainer'>Items</div>
+const Item: React.FC<{ name: string }> = (props) =>{
+    return <div className="Item">{props.name}</div>
 }
 
 export default ItemsContainer;
